23f061681c8c84dd7b913241b7955623eb4bfe18
blue .EVE <blue .EVE>
2017-09-21 22:01:38 +0800

    init

---
+++ ES6/EcmaScript6.js
@@ -0,0 +1,52 @@
+/**
+ * Created by w on 2017/9/20.
+ */
+//ES5不支持 声明变量的类型
+//TS里面的功能
+let name="小明";
+name=30;
+const HOST="localhost";
+
+let str=`123
+    1232
+    123456
+    
+    ${name}:cccc`;
+console.log(str);
+
+//支持Unicode类型的字符串
+let message="\s4338\3232f";
+console.log(message);
+
+let str2="asdfg";
+//获取指定位置的ASCII值
+console.log(str2.codePointAt(2));
+let sss=294;
+//把ASCII值转换成字符串
+console.log(String.fromCharCode(101));
+
+//ES5不支持 声明变量的类型
+//TS里面的功能
+let name="小明";
+name=30;
+const HOST="localhost";
+
+let str=`123
+    1232
+    123456
+    
+    ${name}:cccc`;
+console.log(str);
+
+//支持Unicode类型的字符串
+let message="\s4338\3232f";
+console.log(message);
+
+let str2="asdfg";
+//获取指定位置的ASCII值
+console.log(str2.codePointAt(2));
+let sss=294;
+//把ASCII值转换成字符串
+console.log(String.fromCharCode(101));
+
+
